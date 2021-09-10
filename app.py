1627b2871f40a8236706acdee779c6786eac1583
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-09-10 13:37:05 +0000

    Connect to Mongo DB / Deploy to Heroku

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import os
 from flask import (
-    Flask, flash, render_template, redirect,
-    request, session, url_for)
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_wtf.csrf import CSRFProtect
-from datetime import datetime, date
-
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
-csrf = CSRFProtect(app)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -20,3 +16,36 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/events")
+@app.route("/get_events")
+def get_events():
+    events = mongo.db.events.find()
+    length = events.count()
+    return render_template("events.html", events=events, length=length)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
