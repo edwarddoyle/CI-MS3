92422c73ddd595db4a58a4aa73c2a394cf701c89
edwarddoyle <edwardpaul.doyle@gmail.com>
2021-09-13 15:29:13 +0000

    Add session functions

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ def register():
         # session the user
         session["user"] = request.form.get("username").lower()
         flash("Registered Successfully", category="success")
+        return redirect(url_for("reports", username=session["user"]))
 
     return render_template("register.html")
 
@@ -61,9 +62,11 @@ def login():
         if existing_user:
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        flash("Welcome, {}".format(
-                            request.form.get("username")))
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "reports", username=session["user"]))
             else:
                 flash("Incorrect Username and/or Password", category="error")
                 return redirect(url_for("login"))
@@ -73,6 +76,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/reports/<username>", methods=["GET","POST"])
+def reports(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("reports.html", username=username)
 
 
 @app.route("/events")
